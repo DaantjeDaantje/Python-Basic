224bb3117372428d70cbe0bc7107fa518e79e77e
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-12-07 16:33:08 +0100

    vervolg datatypes

---
+++ 2-vervolg datatypes/taak01-lists/antwoorden.py
@@ -0,0 +1,16 @@
+#%%
+maanden=['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'july', 'augustus', 'september', 'oktober', 'november', 'december']
+
+type(maanden)
+len(maanden)
+maanden[6] = "juli"
+print(*maanden, sep = "\n")
+print(maanden[1])
+len(maanden[3])
+
+
+
+
+
+
+# %%
